ed872c46125b6267b748637fe9ccbcf0b59987a8
DipeshPun91 <pundipesh091@gmail.com>
2025-02-09 19:54:52 +0545

    Design Fix

--- js/app.js
+++ js/app.js
@@ -41,3 +41,148 @@ progressCircles.forEach((circle) => {
     circumference - (progress / 100) * circumference
   }`;
 });
+
+// BAR CHART
+const barChartOptions = {
+  series: [
+    {
+      data: [15, 20, 10, 17, 13],
+    },
+  ],
+  chart: {
+    type: "bar",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#246dec", "#00ff99", "#FEC260", "#1597BB", "#03C4A1"],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    categories: ["Monitor", "Laptop", "Phone", "RAM", "SSD"],
+    labels: {
+      style: {
+        colors: "#FFFFFF",
+      },
+    },
+  },
+  yaxis: {
+    title: {
+      text: "Count",
+      style: {
+        color: "#FFFFFF",
+      },
+    },
+    labels: {
+      style: {
+        colors: "#FFFFFF",
+      },
+    },
+  },
+  tooltip: {
+    theme: "dark",
+  },
+};
+
+const barChart = new ApexCharts(
+  document.querySelector("#bar-chart"),
+  barChartOptions
+);
+barChart.render();
+
+// AREA CHART
+const areaChartOptions = {
+  series: [
+    {
+      name: "Purchase",
+      data: [31, 40, 33, 51, 42, 109, 100],
+    },
+    {
+      name: "Sales",
+      data: [15, 35, 50, 32, 34, 52, 41],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "area",
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#00ff99", "#246dec"],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  markers: {
+    size: 0,
+  },
+  yaxis: [
+    {
+      title: {
+        text: "Purchase",
+        style: {
+          color: "#FFFFFF",
+        },
+      },
+      labels: {
+        style: {
+          colors: "#FFFFFF",
+        },
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        text: "Sales",
+        style: {
+          color: "#FFFFFF",
+        },
+      },
+      labels: {
+        style: {
+          colors: "#FFFFFF",
+        },
+      },
+    },
+  ],
+  xaxis: {
+    labels: {
+      style: {
+        colors: "#FFFFFF",
+      },
+    },
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  },
+  legend: {
+    labels: {
+      colors: "#FFFFFF",
+    },
+  },
+};
+
+const areaChart = new ApexCharts(
+  document.querySelector("#area-chart"),
+  areaChartOptions
+);
+areaChart.render();
